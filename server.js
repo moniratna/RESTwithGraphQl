8caf9e63488d00602e03f06d2c53542ff1ba003c
moniratna <moniratna.munshi123@gmail.com>
2021-07-02 14:05:26 +0530

    2nd commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+const {GraphQLSchema,
+    GraphQLObjectType} = require('graphql');
 
 const app = express();
 
+const schema = new GraphQLSchema({})
+
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}))
 app.listen(5000., ()=> console.log('server running'));
